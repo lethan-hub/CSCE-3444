a93f1d8b8bd53f25d33a71f5435fdae0afe6ed36
lethan-hub <lethan.nguyen12@gmail.com>
2025-06-26 17:06:31 -0500

    Create personal_health.py
    Created and added the functions for the personal health class which takes in the name, age, gender, height, and weight to calculate the BMI and outputs the health status of the person. The users profile is stored inside of a dictionary that is outputted.

---
+++ personal_health.py
@@ -0,0 +1,40 @@
+class PersonalHealth:
+  def __init__(self,name,age,gender,height_cm,weight_kg):
+    self.name = name
+    self.age = age
+    self.gender = gender
+    self.height_cm = height_cm
+    self.weight_kg = weight_kg
+    self.bmi = self.calculate_bmi()
+    self.status = self.get_health_status()
+
+  def calculate_bmi(self):
+    height_m = self.height_cm / 100
+    return round(self.weight_kg / (height_m ** 2), 2)
+  
+  def health_status(self):
+    bmi = self.bmi
+    if bmi < 18.5:
+      return "Underweight"
+    elif 18.5 <= bmi < 24.9:
+      return "Normal weight"
+    elif 25<= bmi < 29.9:
+      return "Overweight"
+    else:
+      return "Obese"
+
+    def update_weight(self, new_weight_kg):
+      self.weight_kg = new_weight_kg
+      self.bmi = self.caclulate_bmi()
+      self.status = self.get_health_status()
+
+    def profle(self):
+      return {
+          "Name" : self.name,
+          "Age" : self.age,
+          "Gender" : self.gender,
+          "Height (cm)" : self.height_cm,
+          "Weight (kg)" : self.weight_kg,
+          "BMI" : self.bmi,
+          "Health Status" : self.status
+      }
